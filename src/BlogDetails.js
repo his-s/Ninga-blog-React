aad1def95c72f96d224cc68f09477456b6d6f3d5
his-s <hishamy42@yahoo.com>
2022-03-13 02:13:58 +0200

    complete all lesson :) ,awesome course and finished in 2 days

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom";
 
 const BlogDetails = () => {
   const { id } = useParams();
@@ -8,6 +9,14 @@ const BlogDetails = () => {
     isPending,
     error,
   } = useFetch("http://localhost:8000/blogs/" + id);
+  const history = useHistory();
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
   return (
     <div className="blog-details">
       {isPending && <div>Loadding...</div>}
@@ -17,6 +26,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
